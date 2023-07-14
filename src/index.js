067966b05ea1fbc48195d2d8458479f3638bc037
ddaskol <kdk.fashik@gmail.com>
2023-07-14 14:08:34 +0300

    create Link, Text and Hi components

--- src/index.js
+++ src/index.js
@@ -1,17 +1,59 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from 'react-dom'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// варіант 1
+// створення React element через функцію Create element
+// const el = React.createElement(
+// 	'div',
+// 	{
+// 		id: "test",
+// 		name: "user",
+// 	},
+// 	React.createElement("h1", null, "Hello world")
+// )
+// варіант 1 
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// варіант 2 через JSX
+const Hi = () => { // React компонент повинен починатися з великої літери
+	return (
+		<h1>Hello WOrld22</h1>
+	)
+}
+const Ul = () => { // React компонент повинен починатися з великої літери
+	return (
+		<ul>
+			<li>1</li>
+			<li>2</li>
+			<li>3</li>
+			<li>4</li>
+			<li>5</li>
+		</ul>
+	)
+}
+const Text = () => { // React компонент повинен починатися з великої літери
+	return (
+		<React.Fragment>
+			<p>lorem</p>
+			<p>Ipsum</p>
+			<p>SIt amet dolor</p>
+		</React.Fragment>
+		//також можна прописувати React.Fragment простіше <></>
+	)
+}
+
+const App = () => {
+	return (
+		<>
+			<Ul />
+			<Text />
+			<Hi />
+		</>
+	)
+}
+
+// variant2 
+
+
+
+ReactDOM.render(<App />, document.getElementById("root"))
+//
