174527fd159816d2e26d57e8e1cda3e8e79f4fc4
ddaskol <kdk.fashik@gmail.com>
2023-08-11 15:13:32 +0300

    add Products[] from PrList

--- src/App/Main/Products/ProductList.js
+++ src/App/Main/Products/ProductList.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ProductListItem from "./ProductListItem";
+import Products from "./Products";
 
 
 const ProductList = () => {
@@ -7,34 +8,21 @@ const ProductList = () => {
         <>
             <h1 className="page_title"> Product List</h1>
             <div className="product_list">
-                <ProductListItem
-                    name="iPhone X"
-                    description="This is iPhone X"
-                    type="phone"
-                    capacity="64gb"
-                    price="1000"
-                />
-                <ProductListItem
-                    name="Iphone XS"
-                    description="This is iPhone XS"
-                    type="phone"
-                    capacity="64"
-                    price="1000"
-                />
-                <ProductListItem
-                    name="Iphone 12"
-                    description="This is iPhone 12"
-                    type="phone"
-                    capacity="64"
-                    price="1000"
-                />
-                <ProductListItem
-                    name="Iphone 13"
-                    description="This is iPhone 13"
-                    type="phone"
-                    capacity="64"
-                    price="1000"
-                />
+                {
+                    Products.map(({
+                        id, name, description, type, capacity, price
+                    }) => (
+                        <ProductListItem
+                            key={id}
+                            name={name}
+                            description={description}
+                            type={type}
+                            capacity={capacity}
+                            price={price}
+                        />
+                    ))
+                }
+
             </div>
         </>
     )
---
+++ src/App/Main/Products/Products.js
@@ -0,0 +1,38 @@
+// import React from "react";
+
+const Products = [
+    {
+        id: 1,
+        name: "Iphone X",
+        description: "This is iPhone X",
+        type: "phone",
+        capacity: "64",
+        price: "1000",
+    },
+    {
+        id: 2,
+        name: "Iphone XS",
+        description: "This is iPhone XS",
+        type: "phone",
+        capacity: "64",
+        price: "1000",
+    },
+    {
+        id: 3,
+        name: "Iphone 13",
+        description: "This is iPhone 13",
+        type: "phone",
+        capacity: "64",
+        price: "1000",
+    },
+    {
+        id: 4,
+        name: "Iphone 12",
+        description: "This is iPhone 12",
+        type: "phone",
+        capacity: "64",
+        price: "1000",
+    },
+]
+
+export default Products
