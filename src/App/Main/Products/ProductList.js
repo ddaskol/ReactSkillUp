55c7f121cb27999574d3ea93e698851c025f74d1
ddaskol <kdk.fashik@gmail.com>
2023-08-11 13:49:42 +0300

    add props. for Items

--- src/App/Main/Products/ProductList.js
+++ src/App/Main/Products/ProductList.js
@@ -7,10 +7,34 @@ const ProductList = () => {
         <>
             <h1 className="page_title"> Product List</h1>
             <div className="product_list">
-                <ProductListItem />
-                <ProductListItem />
-                <ProductListItem />
-                <ProductListItem />
+                <ProductListItem
+                    name="iPhone X"
+                    description="This is iPhone X"
+                    type="phone"
+                    capacity="64gb"
+                    price="1000"
+                />
+                <ProductListItem
+                    name="Iphone XS"
+                    description="This is iPhone XS"
+                    type="phone"
+                    capacity="64"
+                    price="1000"
+                />
+                <ProductListItem
+                    name="Iphone 12"
+                    description="This is iPhone 12"
+                    type="phone"
+                    capacity="64"
+                    price="1000"
+                />
+                <ProductListItem
+                    name="Iphone 13"
+                    description="This is iPhone 13"
+                    type="phone"
+                    capacity="64"
+                    price="1000"
+                />
             </div>
         </>
     )
--- src/App/Main/Products/ProductListItem.js
+++ src/App/Main/Products/ProductListItem.js
@@ -1,14 +1,14 @@
 import React from "react";
 import "./ProductListItem.css"
 
-const ProductListItem = () => {
+const ProductListItem = (props) => {
     return (
         <div className="product_list_item">
-            <div className="product_title">iPhoneX</div>
-            <div className="product_description"> This is IphoneX</div>
-            <div className="product_features"> Type: phone</div>
-            <div className="product_features"> Capacity: 64gb</div>
-            <div className="product_price"> 1000$</div>
+            <div className="product_title">{props.name}</div>
+            <div className="product_description">{props.description}</div>
+            <div className="product_features">Type: {props.type} </div>
+            <div className="product_features"> Capacity: {props.capacity} Gb</div>
+            <div className="product_price">{props.price} $</div>
             <button className="btn_add_to_cart"> Add to cart</button>
         </div>
     )
