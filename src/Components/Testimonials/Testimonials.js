94e421da574988e2757490d9eb63e542b9d5c488
ddaskol <kdk.fashik@gmail.com>
2023-09-19 15:18:56 +0300

    add changeColor fn

--- src/Components/Testimonials/Testimonials.js
+++ src/Components/Testimonials/Testimonials.js
@@ -3,10 +3,23 @@ import React, { useState } from "react";
 const Testimonials = () => {
 
     const [count, setCount] = useState(1);
+    const [color, setColor] = useState('green')
+
+    const toggleColor = () => {
+        setColor((value) => (
+            value === 'red' ? 'green' : 'red'
+        ))
+    }
     return (
         <>
             <h1 > Testimonials</h1>
 
+            <div>
+                Color: {color}
+                <div>
+                    <button onClick={() => toggleColor()}> Change color</button>
+                </div>
+            </div>
             <button onClick={() => setCount(count - 1)}>-</button>
             <input type="text" value={count} />
             <button onClick={() => setCount(count + 1)}>+</button>
