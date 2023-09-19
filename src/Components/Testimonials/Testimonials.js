087c848f0b3a688f9d85632ffc5e03c9a82f5289
ddaskol <kdk.fashik@gmail.com>
2023-09-19 16:25:11 +0300

    add hooks

--- src/Components/Testimonials/Testimonials.js
+++ src/Components/Testimonials/Testimonials.js
@@ -2,27 +2,80 @@ import React, { useState } from "react";
 
 const Testimonials = () => {
 
-    const [count, setCount] = useState(1);
-    const [color, setColor] = useState('green')
+    const arrTestimonials = [
+        {
+            name: "Jack", text: "Lorem ipsum set amet alfa tauris f1 team for london race winLorem ipsum set amet alfa tauris f1 team for london race win Lorem ipsum set amet alfa tauris f1 team for london race win Lorem ipsum set amet alfa tauris f1 team for london race win"
+        },
+        {
+            name: "Anastasia", text: "Lorem ipsum set amet alfa tauris f1 team for london race winLorem ipsum set amet alfa tauris f1 team for london race win Lorem ipsum set amet alfa tauris f1 team for london race win Lorem ipsum set amet alfa tauris f1 team for london race win"
+        },
+        {
+            name: "Anastasia", text: "Lorem ipsum set amet alfa tauris f1 team for london race winLorem ipsum set amet alfa tauris f1 team for london race win Lorem ipsum set amet alfa tauris f1 team for london race win Lorem ipsum set amet alfa tauris f1 team for london race win"
+        },
+        {
+            name: "Anastasia", text: "Lorem ipsum set amet alfa tauris f1 team for london race winLorem ipsum set amet alfa tauris f1 team for london race win Lorem ipsum set amet alfa tauris f1 team for london race win Lorem ipsum set amet alfa tauris f1 team for london race win"
+        },
+        {
+            name: "Anastasia", text: "Lorem ipsum set amet alfa tauris f1 team for london race winLorem ipsum set amet alfa tauris f1 team for london race win Lorem ipsum set amet alfa tauris f1 team for london race win Lorem ipsum set amet alfa tauris f1 team for london race win"
+        },
+    ]
 
-    const toggleColor = () => {
-        setColor((value) => (
-            value === 'red' ? 'green' : 'red'
-        ))
+    const [newTestimonials, setNewTestimonials] = useState({
+        name: "",
+        text: "",
+    })
+
+    const handleNameChange = (e) => {
+        setNewTestimonials((value) => ({
+            ...value,
+            name: e.target.value
+        }))
     }
+
+    const handleTextChange = (e) => {
+        setNewTestimonials((value) => ({
+            ...value,
+            text: e.target.value
+        }))
+    }
+    console.log(newTestimonials)
     return (
         <>
             <h1 > Testimonials</h1>
 
-            <div>
-                Color: {color}
+
+            {
+                arrTestimonials.map((item) => (
+                    <div style={{
+                        backgroundColor: "red",
+                        padding: "10px"
+                    }}>
+                        <div> Name: {item.name}</div>
+                        <div> Message: {item.text}</div>
+                    </div>
+                ))
+            }
+
+            <form>
                 <div>
-                    <button onClick={() => toggleColor()}> Change color</button>
+                    <input
+                        type="text"
+                        placeholder="Your Name"
+                        value={newTestimonials.name}
+                        onChange={handleNameChange}
+                    />
                 </div>
-            </div>
-            <button onClick={() => setCount(count - 1)}>-</button>
-            <input type="text" value={count} />
-            <button onClick={() => setCount(count + 1)}>+</button>
+                <div>
+                    <textarea
+                        cols={30} rows={10}
+                        placeholder="Your Message"
+                        value={newTestimonials.text}
+                        onChange={handleTextChange}
+                    ></textarea>
+                </div>
+                <button> Leave a comment</button>
+            </form>
+
         </>
     )
 }
